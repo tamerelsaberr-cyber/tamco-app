d86b08aaa67a20613960ea6aff2e68783c4eeb81
tamer <tamerelsaberr@gmail.com>
2026-08-21 04:11:41 +0300

    fix: pristine middleware setup

--- middleware.js
+++ middleware.js
@@ -1,14 +1,20 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  const response = NextResponse.next();
+  const url = request.nextUrl.clone();
   
-  // تفعيل الـ CORS لتجاوز حظر متصفح Pi
+  if (url.pathname !== '/' && !url.pathname.includes('.')) {
+    url.pathname = '/';
+    return NextResponse.rewrite(url);
+  }
+
+  const response = NextResponse.next();
   response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
   return response;
+}
+
 export const config = {
   matcher: '/:path*',
 };
