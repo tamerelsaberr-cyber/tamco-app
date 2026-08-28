3c07cabec7c999b90f7ecc3a6223f3cd4507734b
تامر احمد يوسف الصابر <tamerelsaberr@gmail.com>
2026-08-28 15:26:22 +0400

    Add CORS support for OPTIONS request

---
+++ app/api/payments/route.js
@@ -0,0 +1,10 @@
+export async function OPTIONS(request) {
+  return new Response(null, {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Hex-Nonce',
+    },
+  });
+}
